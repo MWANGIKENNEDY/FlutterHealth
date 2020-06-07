55493e1d9c3ee23afcd558fc9a1d5ae56a8ca780
MWANGIKENNEDY <kenmwan75@gmail.com>
2020-06-07 10:18:07 +0300

    Project setup

---
+++ android/app/src/main/kotlin/com/example/flutterappp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterappp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page/home_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    var height=MediaQuery.of(context).size.height;
+    // TODO: implement build
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0.0,
+            height: height*0.35,
+            child: ClipRRect(
+              borderRadius: BorderRadius.vertical(
+                bottom: Radius.circular(60.0)
+              ),
+              child: Container(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Positioned(
+            top: height*0.38,
+            left: 0,
+            right: 0,
+            child: Container(
+              color: Colors.teal,
+              height: height,
+              child: Column(
+                children: <Widget>[
+
+                ],
+              ),
+            ),
+          )
+        ],
+      )
+    );
+  }
+}
